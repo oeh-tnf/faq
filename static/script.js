929f4d6a35c608b2e399902b5211c0daecc0b405
jstsmthrgk <jstsmthrgk@jstsmthrgk.eu>
2025-03-20 15:10:10 +0100

    use actual icons

--- static/script.js
+++ static/script.js
@@ -2,24 +2,28 @@ window.addEventListener("load", () => {
     const nav = document.querySelector("body nav");
     const nav_button = document.createElement("button");
     nav_button.classList.add("nav-button");
-    nav_button.textContent = "\u2630";
+    nav_button.textContent = "menu";
     nav_button.type = "button";
     nav_button.addEventListener("click", () => {
         if (!nav.classList.contains("shown")) {
             nav.classList.add("shown");
             nav.classList.add("shown-at-least-once");
-            nav_button.textContent = "\u{1F7A9}";
+            nav.ariaHidden = false;
+            nav_button.textContent = "close";
         } else {
             nav.classList.remove("shown");
-            nav_button.textContent = "\u2630";
+            nav.ariaHidden = true;
+            nav_button.textContent = "menu";
         }
     });
-    document.body.appendChild(nav_button);
+    document.body.insertBefore(nav_button, nav);
     for (const link of document.querySelectorAll("nav a")) {
         link.addEventListener("click", () => {
             nav.classList.remove("shown");
-            nav_button.textContent = "\u2630";
+            nav.ariaHidden = true;
+            nav_button.textContent = "menu";
         })
     }
     nav.classList.add("scripted");
+    nav.ariaHidden = true;
 });
