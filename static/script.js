45d6cd4f13c65f6582570da239f9e331cce88a73
jstsmthrgk <jstsmthrgk@jstsmthrgk.eu>
2025-03-20 15:25:26 +0100

    fix tabindex

--- static/script.js
+++ static/script.js
@@ -4,26 +4,39 @@ window.addEventListener("load", () => {
     nav_button.classList.add("nav-button");
     nav_button.textContent = "menu";
     nav_button.type = "button";
+
+    function show() {
+        nav.classList.add("shown");
+        nav.classList.add("shown-at-least-once");
+        nav.ariaHidden = false;
+        nav_button.textContent = "close";
+        for (const link of nav.getElementsByTagName("a")) {
+            link.tabIndex = null;
+        }
+    }
+
+    function hide() {
+        nav.classList.remove("shown");
+        nav.ariaHidden = true;
+        nav_button.textContent = "menu";
+        for (const link of nav.getElementsByTagName("a")) {
+            link.tabIndex = -1;
+        }
+    }
+
     nav_button.addEventListener("click", () => {
         if (!nav.classList.contains("shown")) {
-            nav.classList.add("shown");
-            nav.classList.add("shown-at-least-once");
-            nav.ariaHidden = false;
-            nav_button.textContent = "close";
+            show();
         } else {
-            nav.classList.remove("shown");
-            nav.ariaHidden = true;
-            nav_button.textContent = "menu";
+            hide();
         }
     });
     document.body.insertBefore(nav_button, nav);
     for (const link of document.querySelectorAll("nav a")) {
         link.addEventListener("click", () => {
-            nav.classList.remove("shown");
-            nav.ariaHidden = true;
-            nav_button.textContent = "menu";
+            hide();
         })
     }
     nav.classList.add("scripted");
-    nav.ariaHidden = true;
+    hide();
 });
